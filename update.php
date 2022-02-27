ea71d42be3ab7ee18c4e7991febb1b5d1b5acc08
冰雪殇璃陌梦 <80821706+DreamOfIce@users.noreply.github.com>
2022-02-27 13:37:51 +0800

    Update update.php
    Former-commit-id: 7ba852af5a182a11f8b431753d6f03aa8ed5de28

--- update.php
+++ update.php
@@ -1,5 +1,18 @@
 <?php
 
+//get all headers
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 //curl get function
 function curlGet($url, $ua, $auth = '')
 {
