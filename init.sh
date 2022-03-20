c13fba797996b871dc1fa45cace822adec3fa7e0
DreamOfIce <dreamofice@outlook.com>
2022-03-20 12:50:14 +0800

    为init.sh添加环境变量检查
    Former-commit-id: 413b9e9188e34ab67bbf9ed087247b94dbaa672a

--- init.sh
+++ init.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 #此脚本用于首次获取资源列表
 
+#检查环境变量
+if [ -z "${GITHUB_AUTH}"]; then
+    echo "[WARN]您未将Github Personal Access Token添加到环境变量中,会有60Req/h/IP的速率限制,详见 https://github.com/DreamOfIce/HoYoRandom-php/blob/main/readme.md#环境变量"
+fi
+
+if [ -z "${WEBHOOK_SECRET}" ]; then
+    echo "[WARN]您未设置Webhook验证,请勿在生产环境中使用此配置,详见 https://github.com/DreamOfIce/HoYoRandom-php/blob/main/readme.md#环境变量"
+fi
+
+if [ -z "$RES_REPO_NAME" ]; then
+    echo "[WARN]未设置资源仓库,使用默认值'DreamOfIce/HoYoRandomResources' !"
+fi
+
 #添加PHP到PATH(适用于heroku系)
 type php >/dev/null
 if [ ! $? ]; then
-    echo 'add php to path'
+    echo 'Add PHP to path'
     export PATH="${PWD}/.heroku/php/bin"
 fi
-#执行脚本
+
+#执行update.php
 php ./update.php
 
+echo 'Done!'
