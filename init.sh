a89cf020d19fab6d360b7b094966e4c2b0b0ace2
DreamOfIce <dreamofice@outlook.com>
2022-03-20 12:54:33 +0800

    修复./init.sh: line 5: [: missing `]'
    Former-commit-id: 9185c691092f103d4eea2407e573cf99e2e352b8

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #此脚本用于首次获取资源列表
 
 #检查环境变量
-if [ -z "${GITHUB_AUTH}"]; then
+if [ -z "${GITHUB_AUTH}" ]; then
     echo "[WARN]您未将Github Personal Access Token添加到环境变量中,会有60Req/h/IP的速率限制,详见 https://github.com/DreamOfIce/HoYoRandom-php/blob/main/readme.md#环境变量"
 fi
 
